3f3685d0b997aa8d4861b2d1875a1b995880b8e5
Sadoune <Sadounefadjre321@gmail.com>
2026-04-12 21:26:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,33 @@
+import io
+import torch
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from PIL import Image
-import torch
-import timm
-import io
+
+from config import CLASSES, DEVICE, TRANSFORM, load_model
 
 app = FastAPI(title="Skin Cancer Detection API")
 
-# ---------- Configuration ----------
-DEVICE = "cpu"
-NUM_CLASSES = 7
-MODEL_PATH = "best_model.pth"   
 
-# ---------- Load Model ----------
-model = timm.create_model(
-    "efficientnet_b0",
-    pretrained=False,
-    num_classes=NUM_CLASSES
-)
+model = load_model()
 
-checkpoint = torch.load(MODEL_PATH, map_location=DEVICE)
-model.load_state_dict(checkpoint)
-model.to(DEVICE)
-model.eval()
-
-# ---------- Classes ----------
-classes = [
-    "akiec",  # Actinic keratoses
-    "bcc",    # Basal cell carcinoma
-    "bkl",    # Benign keratosis
-    "df",     # Dermatofibroma
-    "mel",    # Melanoma
-    "nv",     # Melanocytic nevus
-    "vasc"    # Vascular lesion
-]
-
-# ---------- Image Preprocessing ----------
-from torchvision import transforms
-
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-])
-
-# ---------- Routes ----------
 
 @app.get("/")
 def home():
-    return {"message": "Skin Cancer API is running 🚀"}
+    return {"message": "Skin Cancer API is running "}
+
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     try:
-        # Read image
+        
         image_bytes = await file.read()
         image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
 
-        # Preprocess
-        img_tensor = transform(image).unsqueeze(0).to(DEVICE)
+        
+        img_tensor = TRANSFORM(image).unsqueeze(0).to(DEVICE)
 
-        # Prediction
+       
         with torch.no_grad():
             outputs = model(img_tensor)
             probs = torch.softmax(outputs, dim=1)
@@ -67,7 +35,7 @@ async def predict(file: UploadFile = File(...)):
             confidence = probs[0][pred].item()
 
         return JSONResponse({
-            "prediction": classes[pred],
+            "prediction": CLASSES[pred],
             "confidence": round(confidence, 4)
         })
 
